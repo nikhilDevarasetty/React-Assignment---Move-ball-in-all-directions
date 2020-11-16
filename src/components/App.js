84aa5476600ea5f8d4b3dc3f7ce45db252a8e2ce
nikhilDevarasetty <nicholus158@gmail.com>
2020-11-16 11:33:43 +0530

    testing1

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,69 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
 //comment
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
 
+  const reset = () => {
+    setRenderBall(false);
+  };
+
+  const start = () => {
+    setRenderBall(true);
+  };
+
+  const renderChoice = () => {
+    if (!renderBall) {
+      return (
+        <button className="start" onClick={start}>
+          start
+        </button>
+      );
+    } else {
+      // console.log(x, y);
+      return (
+        <div
+          className="ball"
+          style={{ left: ballPosition.left, top: ballPosition.top }}
+        ></div>
+      );
+    }
+  };
+
+  const action = (event) => {
+    let x1, y1;
+    if (event.key === "ArrowRight") {
+      x1 = x + 5;
+      y1 = y;
+      setX(x1);
+      setBallPosition({ left: x1 + "px", top: y1 + "px" });
+    } else if (event.key === "ArrowLeft") {
+      x1 = x - 5;
+      y1 = y;
+      setX(x1);
+      setBallPosition({ left: x1 + "px", top: y1 + "px" });
+    } else if (event.key === "ArrowUp") {
+      x1 = x;
+      y1 = y - 5;
+      setX(y1);
+      setBallPosition({ left: x1 + "px", top: y1 + "px" });
+    } else if (event.key === "ArrowDown") {
+      x1 = x;
+      y1 = y + 5;
+      setX(y1);
+      setBallPosition({ left: x1 + "px", top: y1 + "px" });
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", action);
+  });
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
